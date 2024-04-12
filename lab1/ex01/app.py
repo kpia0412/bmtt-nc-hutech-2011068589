a82c42d6edcebbd3ffb173354a88dbaeb5d8be4b
luuvinh <nguyenluuvinh02@gmail.com>
2024-04-12 11:04:44 +0700

    update week6

--- lab1/ex01/app.py
+++ lab1/ex01/app.py
@@ -15,22 +15,22 @@ def caesar():
 
 @app.route("/encrypt", methods=['POST'])
 def caesar_encrypt():
-    text = request.form.get('inputPlainText', '')  # Lấy giá trị của trường 'inputPlainText' từ form
-    key = request.form.get('inputKey')  # Lấy giá trị của trường 'inputKey' từ form
+    text = request.form.get('inputPlainText', '')  
+    key = request.form.get('inputKey')  
     if key is None:
-        return "Key is missing"  # Trả về thông báo nếu key không được gửi
-    key = int(key)  # Chuyển đổi key thành số nguyên
+        return "Key is missing" 
+    key = int(key) 
     caesar = CaesarCipher()
     encrypted_text = caesar.encrypt_text(text, key)
     return f"text: {text}<br/>key: {key}<br/>encrypted text: {encrypted_text}"
 
 @app.route("/decrypt", methods=['POST'])
 def caesar_decrypt():
-    text = request.form.get('inputCipherText', '')  # Lấy giá trị của trường 'inputCipherText' từ form
-    key = request.form.get('inputKeyCipher')  # Lấy giá trị của trường 'inputKeyCipher' từ form
+    text = request.form.get('inputCipherText', '') 
+    key = request.form.get('inputKeyCipher') 
     if key is None:
-        return "Key is missing"  # Trả về thông báo nếu key không được gửi
-    key = int(key)  # Chuyển đổi key thành số nguyên
+        return "Key is missing" 
+    key = int(key)  
     caesar = CaesarCipher()
     decrypted_text = caesar.decrypt_text(text, key)
     return f"text: {text}<br/>key: {key}<br/>decrypted text: {decrypted_text}"
